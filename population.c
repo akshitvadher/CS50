d8cf099c3fc12cbe2af76e1b2d8b6a5d303e4c5c
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-06-04 13:11:33 +0000

    Sun, Jun 4, 2023, 6:11 AM -07:00

--- population.c
+++ population.c
@@ -6,12 +6,27 @@ int main(void)
 
      // prompt of sstart llamas
 
+     int start;
+
+     do{
+
+            start=get_int(" enter start : ");
+
+     } while( start < 9);
+
      //prompt of end llamas
+
+        int end;
+     do{
+
+            end=get_int(" enter end : ");
+
+     } while( end < start);
 
      //how many yreas to reach the goal
 
      //every year 1/3 born  && 1/4 pass away
 
-     
+
 
 }
