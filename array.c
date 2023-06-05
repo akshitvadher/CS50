b26f37bc7b4c7c1a342dc72b6c8b4d5f9146b063
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-06-05 04:54:21 +0000

    Sun, Jun 4, 2023, 9:54 PM -07:00

---
+++ array.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+    int n = get_int(" size of array :");
+
+    int a[n];
+
+     for(imt i = 0 ; i < n ; i++)
+        {
+            printf()
+        }
+
+}
