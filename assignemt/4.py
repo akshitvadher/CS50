cc1dafc686588f80b0bcadf6654fa9fa66e6010d
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-14 00:44:41 +0000

    Thu, Jul 13, 2023, 5:44 PM -07:00

--- assignemt/4.py
+++ assignemt/4.py
@@ -1,1 +1,10 @@
-n=int(input())
+n=int(input("Enter a number :"))
+
+if(n < 0):
+    print("Number is LESSTHEN Zero")
+elif( n > 0):
+    print("Number is GREATENTHEN Zero")
+else:
+    print("Number is Zero")
+
+
