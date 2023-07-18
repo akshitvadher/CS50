4a25de564bd973271b742e9a365d93c529105830
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-18 08:13:05 +0000

    Tue, Jul 18, 2023, 1:13 AM -07:00

--- playback/playback.py
+++ playback/playback.py
@@ -1,3 +1,3 @@
-print("This    is   CS50")
-print("This    is   our     week    on      function")
-print("Let's   implement   a   function    called  hello")
+print("This is CS50")
+print("This is our week on function")
+print("Let's implement a function called hello")
