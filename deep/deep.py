66eb7540fb1c2f64ebdcb29e4b7707102e71876e
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-21 09:52:27 +0000

    Fri, Jul 21, 2023, 3:22 PM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -2,8 +2,9 @@ n = input(" What is the answer to the Great Question of Life, the Universe and E
 
 if ( n == 42 ):
     print("Yes ")
+
 elif( n == forty-two ):
     print("Yes ")
+
 elif( n == forty two ):
     print("Yes ")
-e
