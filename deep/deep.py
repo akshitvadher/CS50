dd3a3b605bbd4d0fde2e8eaadbb2ca954f914d23
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-21 10:23:38 +0000

    Fri, Jul 21, 2023, 3:53 PM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -2,13 +2,13 @@ n = input(" What is the answer to the Great Question of Life, the Universe and E
 
 n.strip()
 
-if  n.strip == "42 ":
+if  n.strip() == "42 ":
     print("Yes ")
 
-elif n.lower == 'forty-two' :
+elif n.lower() == 'forty-two' :
     print("Yes ")
 
-elif n.lower == 'forty two' :
+elif n.lower() == 'forty two' :
     print("Yes ")
 
 else :
