7c78f06acb3dec878bf45feaf727a9bd90f44235
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-25 05:45:22 +0000

    Tue, Jul 25, 2023, 11:15 AM +05:30

---
+++ assignemt/8.py
@@ -0,0 +1,9 @@
+str='abcdefghigklmnopdrstuvwxyz'
+
+
+
+for i in range(len(str)):
+    if(i%2==0):
+        print(str[i],end=' ')
+
+print()
---
+++ assignemt/9.py
@@ -0,0 +1,8 @@
+str='abcdefghigklmnopdrstuvwxyz'
+
+
+
+for i in range(len(str)):
+    if( i%2 != 0 ):
+        print( str[i], end=' ')
+print()
