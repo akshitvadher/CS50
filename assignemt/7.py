666a29fba2e2fba20e33d3db47c5dc50adc2953d
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-25 05:24:07 +0000

    Tue, Jul 25, 2023, 10:54 AM +05:30

--- assignemt/7.py
+++ assignemt/7.py
@@ -3,5 +3,6 @@
 
 '''write a python program that takes a list of words and returns the length of the list.'''
 
-list ={[rajkot],[surat],[jamnager],[diu],[daman]}
-
+list =[rajkot,surat,jamnager,diu,daman]
+length=len(list)
+print("This is length of list :",length)
