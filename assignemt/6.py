8d892b1fde274653c62a40b85c09f98128be17f8
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-25 05:09:15 +0000

    Tue, Jul 25, 2023, 10:39 AM +05:30

---
+++ assignemt/6.py
@@ -0,0 +1,8 @@
+
+
+'''write a python program to reverse the given string.'''
+
+print("prince")
+st2=input("enter string to reverse it-->")
+print(st2[::-1])
+print()
---
+++ assignemt/7.py
@@ -0,0 +1,9 @@
+
+
+
+'''write a python program that takes a list of words and returns the length of the list.'''
+
+print("prince")
+op=input("enter the words with space for check its length-").split()
+print(f"length of list is {len(op)}")
+print()
