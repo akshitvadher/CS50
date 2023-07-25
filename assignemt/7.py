475c96455051df4f77af114bfb9e09e91ab96671
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-25 05:20:30 +0000

    Tue, Jul 25, 2023, 10:50 AM +05:30

--- assignemt/7.py
+++ assignemt/7.py
@@ -3,4 +3,5 @@
 
 '''write a python program that takes a list of words and returns the length of the list.'''
 
-print("write a python program that takes a list of words and returns the length of the list")
+list ={[rajkot],[surat],[jamnager],[diu],[daman]}
+
