4870bda9fd6108dd206d6f412a8704f197dd7dc6
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-25 05:34:46 +0000

    Tue, Jul 25, 2023, 11:04 AM +05:30

--- assignemt/7.py
+++ assignemt/7.py
@@ -4,9 +4,10 @@
 '''write a python program that takes a list of words and returns the length of the list.'''
 
 list =['rajkot','surat','jamnager','diu','daman']
+
 '''length=len(list)
 print("This is list of : "+ str(length) + " Eelements")'''
 
 for n in list:
     a = len(n)
-    print("the lenght of " + n + " is " + a )
+    print("the lenght of " + n + " is " , a )
