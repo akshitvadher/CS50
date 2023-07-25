df4f79d8b1ef5dbc329da89991eb0a8807b4cfe8
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-25 05:12:55 +0000

    Tue, Jul 25, 2023, 10:42 AM +05:30

--- assignemt/6.py
+++ assignemt/6.py
@@ -2,7 +2,7 @@
 
 '''write a python program to reverse the given string.'''
 
-print("prince")
-st2=input("enter string to reverse it-->")
+print("AKshit")
+st2=input("enter string to reverse it...")
 print(st2[::-1])
 print()
