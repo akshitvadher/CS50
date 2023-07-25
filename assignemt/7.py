6b22afb03f3e2d25e9dee6fbcfd1e2b088f5811c
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-25 05:10:50 +0000

    Tue, Jul 25, 2023, 10:40 AM +05:30

--- assignemt/7.py
+++ assignemt/7.py
@@ -3,7 +3,4 @@
 
 '''write a python program that takes a list of words and returns the length of the list.'''
 
-print("prince")
-op=input("enter the words with space for check its length-").split()
-print(f"length of list is {len(op)}")
-print()
+print("write a python program that takes a list of words and returns the length of the list")
