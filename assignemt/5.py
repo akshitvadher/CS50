535c496d85ec22e0c96ef08833ca3c0e637264bc
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-07-25 05:15:49 +0000

    Tue, Jul 25, 2023, 10:45 AM +05:30

--- assignemt/5.py
+++ assignemt/5.py
@@ -1,5 +1,5 @@
-Write a Python program to calculate the length of a string
+'''Write a Python program to calculate the length of a string'''
 
 print("Akshit")
-st1="enter a string to calculate its length...."
+st1=("enter a string to calculate its length....")
 print(len(st1))
