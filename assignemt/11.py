fa2f2f13418fba8291b06a76b05aa1fffcd4a939
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-08-01 05:40:19 +0000

    Tue, Aug 1, 2023, 11:10 AM +05:30

--- assignemt/11.py
+++ assignemt/11.py
@@ -1,1 +1,4 @@
 '''Write a python program to find the factorial of a given number.'''
+
+        x = 1
+        The factorial is {[x := x * i for i in range(1, int(input('Enter a number: ')) + 1)][-1]}
