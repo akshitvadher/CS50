9d14a7e2ae130030fdd8480615a103c6a6230628
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-08-19 10:12:26 +0000

    Sat, Aug 19, 2023, 3:42 PM +05:30

--- mario.py
+++ mario.py
@@ -7,10 +7,9 @@ while True:
         break
 
 for i in range(0, n, 1):
-    for j in range(0, n + i + 3, 1):
-        if j == n or j == n + 1 or i + j < n - 1:
-            print(" ", end="")
+    for j in range(0, n, 1):
+        if( i + j < n -1):
+            print("", end="")
         else:
             print("#", end="")
-
     print()
