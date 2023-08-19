dac6b7e8d015f4105b6a29904aaead6eaba31356
akshitvadher <122861906+akshitvadher@users.noreply.github.com>
2023-08-19 09:51:29 +0000

    Sat, Aug 19, 2023, 3:21 PM +05:30

--- mario.py
+++ mario.py
@@ -1,6 +1,13 @@
-for i in range(0, n, 1):
-    for j in range(0, n+i+3, 1):
-        if (j ==  n or j  == n+1 or i+j < n-1):
-            print(" " ,end="")
-        else:
-            print("#" ,end="")
+form cs50 import get_int
+
+
+
+while true 
+
+
+# for i in range(0, n, 1):
+#     for j in range(0, n+i+3, 1):
+#         if (j ==  n or j  == n+1 or i+j < n-1):
+#             print(" " ,end="")
+#         else:
+#             print("#" ,end="")
